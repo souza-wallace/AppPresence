f4f374a6e33420987eeb0a0edd1b0b3578bf5509
souza-wallace <walizdesouza781@gmail.com>
2024-02-13 20:04:42 -0300

    ajuste em msg de erro

--- app/Http/Controllers/PresenceController.php
+++ app/Http/Controllers/PresenceController.php
@@ -22,7 +22,7 @@ class PresenceController extends Controller
         $hasPresenceToday = $result['hasPresenceToday'];
 
         if(count($hasPresenceToday) != 0){
-            return response()->json(['Ja pediu presença na data '.now()->format('Y-m-d')], 405);
+            return 'Ja pediu presença na data '.now()->format('Y-m-d');
         }
 
         $presence->user_id = $user->id;
@@ -46,7 +46,7 @@ class PresenceController extends Controller
         $user = auth()->user();
 
         if($user->role != 'teacher'){
-            return response()->json(['Usuario sem permissáo para realizar essa ação'], 405);
+            return ['Usuario sem permissáo para realizar essa ação'];
         }
 
         $presence->status = 'confirmed';
@@ -61,7 +61,7 @@ class PresenceController extends Controller
         $user = auth()->user();
 
         if($user->role != 'teacher'){
-            return response()->json(['Usuario sem permissáo para realizar essa ação'], 405);
+            return ['Usuario sem permissáo para realizar essa ação'];
         }
 
         $presence->status = 'refused';
@@ -107,8 +107,8 @@ class PresenceController extends Controller
         $pendingUser = 0;
         $today = Carbon::today();
 
-            // $pending = $presence::with('user')->where('status', $this->pending)->where('user_id', $user->id)->get();
-            // $refused = $presence::with('user')->where('status', $this->refused)->where('user_id', $user->id)->whereDate('created_at', $today)->first();
+        // $pending = $presence::with('user')->where('status', $this->pending)->where('user_id', $user->id)->get();
+        // $refused = $presence::with('user')->where('status', $this->refused)->where('user_id', $user->id)->whereDate('created_at', $today)->first();
 
         return response()->json([
             "pending" => $pending ? $pending : 0,
@@ -127,12 +127,12 @@ class PresenceController extends Controller
 
         $presences = $presence::where('status', $this->confirmed)->where('user_id', $user->id)->get();
         $fouls = $presence::where('status', $this->refused)->where('user_id', $user->id)->get();
-        $pending = $presence::where('status', $this->pending)->where('user_id', $user->id)->whereDate('created_at', $today)->get();
+        $pending = $presence::where('status', $this->pending)->where('user_id', $user->id)->whereDate('created_at', $today)->first();
 
         return response()->json([
             "countPresence" => count($presences),
             "countFouls" => count($fouls),
-            "pending" => $pending ? $pending : 0
+            "pending" => $pending ? $pending : []
         ], 200);
 
     }
