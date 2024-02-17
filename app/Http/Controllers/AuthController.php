f6a6009a2d3d9ab81210b926d2688260be53007e
souza-wallace <walizdesouza781@gmail.com>
2024-02-17 15:07:29 -0300

    token expires and migrattin

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -82,7 +82,7 @@ class AuthController extends Controller
             'user' => $user,
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => Carbon::now()->addWeeks(1)
+            'expires_in' => null
         ]);
     }
 }
